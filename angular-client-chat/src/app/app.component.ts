630b488b6304a6134cae0aad550311ed538dd5f9
Samuel Cazelli <samuel@maiorseguros.com.br>
2020-01-29 13:41:14 -0300

    Angular client is now able to send and receive messages

--- angular-client-chat/src/app/app.component.ts
+++ angular-client-chat/src/app/app.component.ts
@@ -1,30 +1,38 @@
-import { Component } from '@angular/core';
-import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
+import { Component, OnInit } from '@angular/core';
+
+import { RealTimeServiceService } from './services/real-time-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularChatClient';
+export class AppComponent implements OnInit {
 
-  private _hubConnection: HubConnection;
+  messages: string[];
 
-
-  constructor() { }
+  constructor(private realTimeService: RealTimeServiceService) {
+    this.messages = [];
+  }
 
   ngOnInit(): void {
-    this._hubConnection =  new HubConnectionBuilder().withUrl("https://localhost:44342/chatHub").build();
-    this._hubConnection
-      .start()
-      .then(() => console.log('Connection started!'))
+
+    this.realTimeService.connect()
+      .then(() => {
+        console.log('Connection started!');
+        this.realTimeService.sendMessage('angular', 'connected');
+      })
       .catch(err => console.log('Error while establishing connection :('));
 
-      this._hubConnection.on('ReceiveMessage', (user: string, message: string) => {
-        console.log(`${user} - ${message}`);
-      });
-    
+    this.realTimeService
+      .onNewMessage = (message) => {
+        this.messages.push(message);
+      };
+
+  }
+
+  handleClickSend(nick: string, message: string) {
+    this.realTimeService.sendMessage(nick, message);
   }
 
 }
