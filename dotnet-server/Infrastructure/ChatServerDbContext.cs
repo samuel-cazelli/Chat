9679baf147725dbb3fcce904df1e9f4a1a868b0b
samuel <samuelcazelli@gmail.com>
2020-03-09 16:52:29 -0300

    Database context

---
+++ dotnet-server/Infrastructure/ChatServerDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ChatServer.Application.Shared;
+using ChatServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatServer.Infrastructure
+{
+    public class ChatServerDbContext : DbContext, IChatServerDbContext
+    {
+
+        public ChatServerDbContext(DbContextOptions<ChatServerDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ChatServerDbContext).Assembly);
+        }
+
+        public DbSet<Message> Messages { get; set; }
+    }
+}
