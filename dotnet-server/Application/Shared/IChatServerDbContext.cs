51493fe6e15f3da2f51e42ceecf461c8b28b029f
samuel <samuelcazelli@gmail.com>
2020-03-09 16:50:41 -0300

    Interface of database context

---
+++ dotnet-server/Application/Shared/IChatServerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ChatServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatServer.Application.Shared
+{
+    public interface IChatServerDbContext
+    {
+
+        DbSet<Message> Messages { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+    }
+}
