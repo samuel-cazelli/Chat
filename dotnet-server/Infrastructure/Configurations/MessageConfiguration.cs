8b51a0ecf24bc7cef547f81ab5c004977061435a
samuel <samuelcazelli@gmail.com>
2020-03-09 16:53:02 -0300

    First migration entity framework

---
+++ dotnet-server/Infrastructure/Configurations/MessageConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using ChatServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatServer.Infrastructure.Configurations
+{
+    public class MessageConfiguration : IEntityTypeConfiguration<Message>
+    {
+
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Nick)
+                .IsRequired()
+                .HasMaxLength(15);
+
+            builder.Property(e => e.Content)
+                .IsRequired()
+                .HasColumnType("ntext");
+
+        }
+
+
+    }
+}
---
+++ dotnet-server/Infrastructure/Migrations/20200309184401_Initial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChatServer.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nick = table.Column<string>(maxLength: 15, nullable: false),
+                    Content = table.Column<string>(type: "ntext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
