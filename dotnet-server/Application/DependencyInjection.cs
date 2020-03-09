cd6cf0ae1c08bfbf91449bc3df1e6c3ae41cab02
samuel <samuelcazelli@gmail.com>
2020-03-09 16:51:39 -0300

    Dependency injection of MediatR

---
+++ dotnet-server/Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ChatServer.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+
+    }
+}
