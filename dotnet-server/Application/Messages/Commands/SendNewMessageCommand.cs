b9bee1e30028f4146095152342a43eb8b022997d
samuel <samuelcazelli@gmail.com>
2020-03-09 16:51:56 -0300

    New message command and handler

---
+++ dotnet-server/Application/Messages/Commands/SendNewMessageCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChatServer.Domain.Entities;
+using MediatR;
+
+namespace ChatServer.Application.Messages.Commands
+{
+    public class SendNewMessageCommand : IRequest<Message>
+    {
+        public SendNewMessageCommand(string nick, string message)
+        {
+            this.Nick = nick;
+            this.Content = message;
+        }
+
+        public string Nick { get; set; }
+
+        public string Content { get; set; }
+
+    }
+}
---
+++ dotnet-server/Application/Messages/Commands/SendNewMessageHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ChatServer.Application.Shared;
+using ChatServer.Domain.Entities;
+using MediatR;
+
+
+
+namespace ChatServer.Application.Messages.Commands
+{
+    class SendNewMessageHandler : IRequestHandler<SendNewMessageCommand, Message>
+    {
+
+        private IChatServerDbContext DbContext { get; set; }
+
+
+        public SendNewMessageHandler(IChatServerDbContext dbContext)
+        {
+            this.DbContext = dbContext;
+        }
+
+        public async Task<Message> Handle(SendNewMessageCommand request, CancellationToken cancellationToken)
+        {
+            var message = new Message
+            {
+                Content = request.Content,
+                Nick = request.Nick
+            };
+
+            DbContext.Messages.Add(message);
+
+            await DbContext.SaveChangesAsync(cancellationToken);
+
+            return message;
+        }
+    }
+}
