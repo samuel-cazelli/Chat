02e008628074da41ed41b0b946446476c8647e0c
samuel <samuelcazelli@gmail.com>
2020-03-09 16:48:14 -0300

    Domain message class

---
+++ dotnet-server/Domain/Entities/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatServer.Domain.Entities
+{
+    public class Message
+    {
+
+        public Int32 Id { get; set; }
+
+        public String Nick { get; set; }
+
+        public String Content { get; set; }
+
+    }
+}
